672d189ec7c1463b6e6e57a2edbc23ac3fb2a65e
Nico <nigr21@inf.ufpr.br>
2023-06-28 21:43:32 -0300

    Added some more tests

--- test/src/backtracking/backtracking-test.cpp
+++ test/src/backtracking/backtracking-test.cpp
@@ -3,7 +3,7 @@
 #include "doctest.h"
 #include "backtracking.h"
 
-TEST_CASE("Backtracking") {
+TEST_CASE("Backtracking case 1") {
   std::cout << "Running: Backtracking:"; 
   std::vector<std::vector<int>> affinities = {
     {0},
@@ -63,5 +63,131 @@ TEST_CASE("Backtracking") {
     int min = backtrackingViabilityAndOptimalityPruning(P, 7, opt);
     CHECK(min == 4);
   }
+  std::cout << "\n";
+}
+
+TEST_CASE("Backtracking case 2") {
+  std::cout << "Running: Backtracking:"; 
+
+  std::vector<std::vector<int>> affinities = {
+    {0},
+    {5},
+    {},
+    {},
+    {},
+    {1},
+  };
+
+  std::vector<std::vector<int>> conflicts = {
+    {0},
+    {2, 3},
+    {1, 3, 4},
+    {1, 2, 4, 5},
+    {2, 3},
+    {3},
+  };
+
+  std::vector<int> g(6, -1);
+
+  Problem P { 
+    .opt = 112345,
+    .number_of_heroes = 5,
+    .conflicts_cnt = 0,
+    .g = g,
+    .affinities = affinities, 
+    .conflicts = conflicts,
+  };
+
+  SUBCASE("No pruning") {
+    std::cout << " [No pruning]";
+    int min = backtrackingNoPruning(P, 5);
+    CHECK(min == 1);
+  } 
+
+  SUBCASE("Viability Pruning") {
+    std::cout << " [Viability pruning]";
+    int min = backtrackingViabilityPruning(P, 5);
+    CHECK(min == 1);
+  }
+  
+  SUBCASE("Optimality Pruning") {
+    std::cout << " [Optimality pruning]";
+    int opt = 112345;
+    int min = backtrackingOptimalityPruning(P, 5, opt);
+    CHECK(min == 1);
+  }
+
+  SUBCASE("Viability and Optimality Pruning") {
+    std::cout << " [Viability and optimality pruning]";
+    int opt = 112345;
+    int min = backtrackingViabilityAndOptimalityPruning(P, 5, opt);
+    CHECK(min == 1);
+  }
+
+  std::cout << "\n";
+}
+
+TEST_CASE("Backtracking case 3") {
+  std::cout << "Running: Backtracking:"; 
+
+  std::vector<std::vector<int>> affinities = {
+    {},
+    {6},
+    {4},
+    {7},
+    {2},
+    {},
+    {1},
+    {3}
+  };
+
+  std::vector<std::vector<int>> conflicts = {
+    {0},
+    {2, 3},
+    {1, 3},
+    {1, 2, 4, 5},
+    {3},
+    {3, 6, 7},
+    {5},
+    {5}
+  };
+
+  std::vector<int> g(8, -1);
+
+  Problem P { 
+    .opt = 112345,
+    .number_of_heroes = 7,
+    .conflicts_cnt = 0,
+    .g = g,
+    .affinities = affinities, 
+    .conflicts = conflicts,
+  };
+
+  SUBCASE("No pruning") {
+    std::cout << " [No pruning]";
+    int min = backtrackingNoPruning(P, 7);
+    CHECK(min == 1);
+  } 
+
+  SUBCASE("Viability Pruning") {
+    std::cout << " [Viability pruning]";
+    int min = backtrackingViabilityPruning(P, 7);
+    CHECK(min == 1);
+  }
+  
+  SUBCASE("Optimality Pruning") {
+    std::cout << " [Optimality pruning]";
+    int opt = 112345;
+    int min = backtrackingOptimalityPruning(P, 7, opt);
+    CHECK(min == 1);
+  }
+
+  SUBCASE("Viability and Optimality Pruning") {
+    std::cout << " [Viability and optimality pruning]";
+    int opt = 112345;
+    int min = backtrackingViabilityAndOptimalityPruning(P, 7, opt);
+    CHECK(min == 1);
+  }
+
   std::cout << "\n";
 }
